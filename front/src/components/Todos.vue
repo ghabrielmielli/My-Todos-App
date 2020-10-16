ff0c780de6a902f7f54d5ef3611d41736ac0f6cd
Ghabriel Mielli <ghabrielmielli@MacBook-Pro-de-Ghabriel.local>
2020-10-16 09:07:45 -0400

    Fetching data from the backend and displaying.

---
+++ front/src/components/Todos.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">{{ todo.name }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  name: "App",
+  data: () => {
+    return {
+      todos: [],
+    };
+  },
+  mounted: function () {
+    axios
+      .get("http://localhost:3000/todos/")
+      .then((res) => {
+        console.log("Fetched data successfully");
+        this.todos = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
+</script>
+<style scoped>
+</style>
