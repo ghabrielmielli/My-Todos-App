745843990b5e21ec30e2f2bd141e4015e2bf38b4
Ghabriel Mielli <ghabrielmielli@MacBook-Pro-de-Ghabriel.local>
2020-10-16 16:54:11 -0400

    presenting todos kinda poorly but fine :p

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,18 +1,38 @@
 <template>
-  <div id="app">
-    <todos></todos>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="white"
+      flat
+    >
+    <v-spacer></v-spacer>
+      <v-card-title primary-title>
+        Todos!
+      </v-card-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    
+    <v-main class="grey lighten-1">
+      <v-container fluid>
+        
+        <Todos></Todos>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import Todos from "./components/Todos.vue"
+import Todos from "./components/Todos";
+  export default {
+    components: {
+      Todos
+    },
+    data: function () {
+      return {
 
-export default {
-  components: {
-    Todos
+      }
+    },
+    
   }
-}
 </script>
-
-<style>
-</style>
--- front/src/components/Todos.vue
+++ front/src/components/Todos.vue
@@ -1,9 +1,44 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id">{{ todo.name }}</li>
-    </ul>
-  </div>
+  <v-row>
+    <v-col cols="3">
+      <v-card flat>
+        <v-toolbar color="grey" dark flat>
+
+          <v-toolbar-title>Inbox</v-toolbar-title>
+
+          <v-spacer></v-spacer>
+
+          <v-btn icon>
+            <v-icon>mdi-plus</v-icon>
+          </v-btn>
+        </v-toolbar>
+
+        <v-list color="transparent">
+          <v-list-item v-for="todo in todos" :key="todo.id" link>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ todo.name }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-col>
+
+    <v-col cols="6">
+      <v-sheet min-height="70vh" rounded="lg">
+        <v-list color="transparent">
+          <v-list-item v-for="todo in todos" :key="todo.id" link>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ todo.name }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-sheet>
+    </v-col>
+  </v-row>
 </template>
 <script>
 import axios from "axios";
