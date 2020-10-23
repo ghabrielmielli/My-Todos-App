e6225bf8122834a319512ff5e9d9f96015f16892
Ghabriel Mielli <ghabrielmielli@MacBook-Pro-de-Ghabriel.local>
2020-10-23 08:57:04 -0400

    Transitions added, posting todos working properly.

--- back/routes/todos.js
+++ back/routes/todos.js
@@ -2,36 +2,30 @@ const router = require("express").Router();
 const db = require("../database/dbconfig");
 
 router.route("/")
-.get((req, res) => {
+	.get((req, res) => {
+		let sql = "SELECT * FROM todo";
 
-    let sql = "SELECT * FROM todo";
+		db.query(sql, (err, results) => {
+			if (err) throw err;
+			res.send(results);
+		});
+	})
+	.post((req, res) => {
+		let sql = "INSERT INTO todo (name, category) VALUES (?, ?)";
+		db.query(sql, [req.body.name, req.body.category], (err, results) => {
+			if (err) throw err;
+			results.message = "Successfully posted new data.";
+			res.send(results);
+		});
+	});
 
-    db.query(sql, (err, results) => {
-        if (err) throw err;
-        res.send(results);
-    })
-})
-.post((req, res) => {
-    let sql = "INSERT INTO todo (name, category) VALUES (?, ?)";
-    console.log("imprimindo valores:");
-    console.log("nome: " + req.body.name);
-    console.log("categoria: " + req.body.category);
-    db.query(sql, [req.body.name, req.body.category], (err, results) => {
-        if (err) throw err;
-        results.message = "Successfully posted new data";
-        res.send(results);
-    })
-    
+router.route("/:id").get((req, res) => {
+	let sql = "SELECT * FROM todo WHERE id = ?";
+
+	db.query(sql, req.params.id, (err, results) => {
+		if (err) throw err;
+		res.send(results);
+	});
 });
 
-router.route("/:id")
-.get((req, res) => {
-    let sql = "SELECT * FROM todo WHERE id = ?";
-    
-    db.query(sql, req.params.id, (err, results) => {
-        if (err) throw err;
-        res.send(results);
-    })
-});
-
-module.exports = router; 
+module.exports = router;
--- front/src/components/Todos.vue
+++ front/src/components/Todos.vue
@@ -1,134 +1,153 @@
 <template>
-  <v-row>
-    <v-col cols="3">
-      <v-card flat>
-        <v-toolbar color="grey" dark flat>
-          <v-toolbar-title>Categories</v-toolbar-title>
+	<v-row>
+		<!-- category section -->
+		<v-col cols="3">
+			<v-card flat>
+				<v-toolbar color="grey" dark flat>
+					<v-toolbar-title>Categories</v-toolbar-title>
 
-          <v-spacer></v-spacer>
+					<v-spacer></v-spacer>
 
-          <v-btn icon>
-            <v-icon>mdi-plus</v-icon>
-          </v-btn>
-        </v-toolbar>
+					<v-btn icon>
+						<v-icon>mdi-plus</v-icon>
+					</v-btn>
+				</v-toolbar>
 
-        <v-list color="transparent">
-          <v-list-item-group color="green">
-            <v-list-item
-              v-for="category in categories"
-              :key="category.id"
-              link
-              @click="attCategorySelected(category.id)"
-            >
-              <v-list-item-content>
-                <v-list-item-title>
-                  {{ category.name }}
-                </v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </v-card>
-    </v-col>
+				<v-list color="transparent">
+					<v-list-item-group color="green" v-model="selectedCategory">
+						<v-list-item v-for="category in categories" :key="category.id" link>
+							<v-list-item-content>
+								<v-list-item-title>
+									{{ category.name }}
+								</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+					</v-list-item-group>
+				</v-list>
+			</v-card>
+		</v-col>
+		<!-- todos section -->
+		<v-col cols="6">
+			<v-card min-height="70vh" rounded="lg">
+				<v-list>
+					<!-- add todos section -->
+					<v-list-item>
+						<v-list-item-title>
+							<v-row dense>
+								<v-col cols="6">
+									<v-text-field
+										:disabled="!selectedCategory"
+										dense
+										append-outer-icon="mdi-plus"
+										placeholder="New todo"
+										@click:append-outer="postNewTodo"
+										@keyup.enter="postNewTodo"
+										v-model="newTodo"
+									></v-text-field>
+								</v-col>
+							</v-row>
+						</v-list-item-title>
+					</v-list-item>
+					<transition-group name="fade" mode="out-in">
+						<!-- items-->
+						<v-list-item v-for="todo in filteredTodos" :key="todo.id" class="fade-item">
+							<v-list-item-action>
+								<v-checkbox v-model="todo.done" color="primary"></v-checkbox>
+							</v-list-item-action>
 
-    <v-col cols="6">
-      <v-sheet min-height="70vh" rounded="lg">
-        <v-list color="transparent">
-          <template v-for="todo in todos">
-            <v-list-item
-              v-if="todo.category == selectedCategory"
-              :key="todo.id"
-              link
-            >
-              <v-list-item-content>
-                <v-list-item-title>
-                  {{ todo.name }}
-                </v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </template>
+							<v-list-item-content>
+								<v-list-item-title>
+									{{ todo.name }}
+								</v-list-item-title>
+							</v-list-item-content>
 
-          <v-list-item v-if="selectedCategory != 0">
-            <v-list-item-title>
-              <v-row dense>
-                <v-col cols="6">
-                  <v-text-field 
-                    dense
-                    append-outer-icon="mdi-plus"
-                    placeholder="New todo"
-                    @click:append-outer="postNewTodo"
-                    @keyup.enter="postNewTodo"
-                    v-model="newTodo"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-            </v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-sheet>
-    </v-col>
-  </v-row>
+							<v-scroll-x-transition>
+								<v-icon v-if="todo.done" color="success">
+									mdi-check
+								</v-icon>
+							</v-scroll-x-transition>
+						</v-list-item>
+					</transition-group>
+				</v-list>
+			</v-card>
+		</v-col>
+	</v-row>
 </template>
 <script>
-import axios from "axios";
+	import axios from "axios";
 
-export default {
-  name: "App",
-  data: () => {
-    return {
-      todos: [],
-      categories: [],
+	export default {
+		name: "App",
+		data: () => {
+			return {
+				i: 1000,
+				todos: [],
+				categories: [],
 
-      selectedCategory: 0,
-      newTodo: "",
-    };
-  },
-  mounted: function () {
-    //FETCH TODOS
-    axios
-      .get("http://localhost:3000/todos/")
-      .then((res) => {
-        console.log("Fetched todos successfully");
-        this.todos = res.data;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+				selectedCategory: 0,
+				newTodo: "",
+			};
+		},
+		mounted: function() {
+			this.fetchTodos();
 
-    //FETCH CATEGORIES
-    axios
-      .get("http://localhost:3000/categories/")
-      .then((res) => {
-        console.log("Fetched categories successfully");
-        this.categories = res.data;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  },
-  methods: {
-    attCategorySelected: function (id) {
-      this.selectedCategory = id;
-    },
-    postNewTodo: function () {
-      var postingTodo = {
-        name: this.newTodo,
-        category: this.selectedCategory,
-      }
-      axios
-      .post("http://localhost:3000/todos/", postingTodo)
-      .then((response) => {
-        console.log("Posted new todo!");
-        console.log("Status " + response.status + " - " + response.data.message);
-        this.todos.push(postingTodo);
-        this.newTodo = "";
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    },
-  },
-};
+			//FETCH CATEGORIES
+			axios.get("http://localhost:3000/categories/")
+				.then((res) => {
+					console.log("Fetched categories successfully");
+					this.categories = res.data;
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		},
+		computed: {
+			filteredTodos: function() {
+				return this.todos.filter((todo) => (this.selectedCategory == 0 ? todo : todo.category == this.selectedCategory));
+			},
+		},
+		methods: {
+			fetchTodos: function() {
+				axios.get("http://localhost:3000/todos/")
+					.then((res) => {
+						console.log("Fetched todos successfully");
+						this.todos = res.data;
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+			},
+			postNewTodo: function() {
+				var postingTodo = {
+					name: this.newTodo,
+					category: this.selectedCategory,
+				};
+				axios.post("http://localhost:3000/todos/", postingTodo)
+					.then((response) => {
+						console.log(response.status + " - " + response.data.message);
+
+						postingTodo.id = response.data.insertId;
+						this.todos.push(postingTodo);
+						this.newTodo = "";
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+			},
+		},
+	};
 </script>
+
 <style scoped>
+	.fade-item {
+		transition: all 0.5s;
+	}
+	.fade-enter, .fade-leave-to
+/* .fade-leave-active below version 2.1.8 */ {
+		opacity: 0;
+		transform: scaleY(0);
+	}
+	.fade-leave-active {
+		position: absolute;
+	}
 </style>
