6c727f5993bc7c033b940b2e200f01beddf12fb5
Ghabriel Mielli <ghabrielmielli@hotmail.com>
2020-11-18 15:58:22 -0400

    tiny bug correction

--- front/src/components/Todos section/ButtonWithTooltip.vue
+++ front/src/components/Todos section/ButtonWithTooltip.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-tooltip bottom open-delay="800">
 		<template v-slot:activator="{ on, attrs }">
-			<v-btn small dark depressed :disabled="disabled" color="primary" v-bind="attrs" v-on="on" @click="$emit('pressed')">
+			<v-btn small depressed :disabled="disabled" color="primary" v-bind="attrs" v-on="on" @click="$emit('pressed')">
 				<v-icon>{{ icon }}</v-icon>
 			</v-btn>
 		</template>
